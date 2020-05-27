58ad9e4d60331e65638cc66de54a002ce13cbe74
Audrey Morin <34172843+msdrey@users.noreply.github.com>
2020-05-26 21:04:20 -0700

    Initial commit

---
+++ MorinCrafts.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using MorinCrafts.Website.Models;
+using MorinCrafts.WebSite.Services;
+
+namespace MorinCrafts.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger, 
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
